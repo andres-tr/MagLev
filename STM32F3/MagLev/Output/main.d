fb0ef636e4f771b892d2c4187ecb8064a74704fd
Andres Tosca <atr.andres@gmail.com>
2014-09-21 02:39:26 -0500

    Fixing .gitignore

--- STM32F3/MagLev/Output/main.d
+++
@@ -1,8 +0,0 @@
-.\output\main.o: Source\main.c
-.\output\main.o: C:\Keil\ARM\Inc\ST\STM32F30x\stm32f30x.h
-.\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cm4.h
-.\output\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-.\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-.\output\main.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
-.\output\main.o: C:\Keil\ARM\Inc\ST\STM32F30x\system_stm32f30x.h
